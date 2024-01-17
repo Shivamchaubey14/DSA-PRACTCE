b8b44a4aff87f9e22f5daa61e84c0dfb585621c1
Shivam Chaubey <shivamc36@gmail.com>
2024-01-17 22:58:59 +0530

    rearranging the array elements by conditions

---
+++ arrays/arrayProblems/13.c
@@ -0,0 +1,41 @@
+//Rearrange array such that even positioned are greater than odd
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void assign(int a[], int n) {
+    // Sort the array
+    qsort(a, n, sizeof(int), compare);
+
+    int* ans = (int*)malloc(n * sizeof(int));
+
+    int p = 0, q = n - 1;
+    for (int i = 0; i < n; i++) {
+        // Assign even indexes with maximum elements
+        if ((i + 1) % 2 == 0)
+            ans[i] = a[q--];
+        // Assign odd indexes with remaining elements
+        else
+            ans[i] = a[p++];
+    }
+
+    // Print result
+    for (int i = 0; i < n; i++)
+        printf("%d ", ans[i]);
+
+    // Free allocated memory
+    free(ans);
+}
+
+// Function to compare two integers for qsort
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+// Driver Code
+int main() {
+    int A[] = { 1, 3, 2, 2, 5 };
+    int n = sizeof(A) / sizeof(A[0]);
+    assign(A, n);
+    return 0;
+}
