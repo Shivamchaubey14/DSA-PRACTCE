7bb63318eda01225b1df15526d3f48e3b7e31707
Shivam Chaubey <shivamc36@gmail.com>
2024-01-19 22:55:59 +0530

    multiply all the elements of list

---
+++ PythonProblems/2.py
@@ -0,0 +1,9 @@
+'''Write a Python Program to multiply all the items in a list'''
+
+def multiplyList(items):
+    total = 1
+    for number in items:
+        total *= 1
+    return total
+
+print(multiplyList[1,2,-8])
